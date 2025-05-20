55c8995d2cd1699b6c7076034e5e7f765333097f
Elya <elyaj.le@gmail.com>
2025-05-20 18:17:54 -0500

    EADDRINUSE: address already in use :::5000

--- src/utils/code-generator.js
+++ src/utils/code-generator.js
@@ -1,22 +1,24 @@
-const { nanoid } = require('nanoid');
+const crypto = require('crypto');
 const db = require('./db');
 
-// function to generate a unique retrieval code
+// Function to generate a unique retrieval code
 const generateUniqueCode = async (length = 8) => {
   let isUnique = false;
   let code;
   
   while (!isUnique) {
-    // generate a code using nanoid
-    code = nanoid(length);
+    // Generate a random code using crypto
+    code = crypto.randomBytes(Math.ceil(length / 2))
+      .toString('hex')
+      .slice(0, length);
     
-    // check if the code already exists in the database
+    // Check if the code already exists in the database
     const result = await db.query(
       'SELECT COUNT(*) FROM card_sets WHERE retrieval_code = $1',
       [code]
     );
     
-    // if count is 0, the code is unique
+    // If count is 0, the code is unique
     if (parseInt(result.rows[0].count) === 0) {
       isUnique = true;
     }
